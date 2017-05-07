68edab41892db145e4e824c72749959f487d1e3e
arvidsigvardsson <arvid.sigvardsson@gmail.com>
2017-05-07 16:29:56 +0200

    Cleaned up rotary7.c

--- rotary7.c
+++ rotary7.c
@@ -1,10 +1,18 @@
+/*
+ *
+ * A program for digitizing input from a rotary phone, and display the result on a 7 segment display, for the Arduino Uno board. A rotary disc works by creating a number of electrical pulses corresponding to the entered digit.
+ * Author: Arvid Sigvardsson
+ *
+ */
+
 #include <avr/io.h>
 #include <util/delay.h>
 
 #define BLINK_DELAY_MS 50
 
-void set7diplay(unsigned char);
+void set_7_display(unsigned char);
 char read_input(void);
+void set_display_position(uint8_t);
 
 enum state {
 	START,
@@ -14,31 +22,47 @@ enum state {
 
 typedef enum state state_dial;
 
+// hexadecimal codes where index 0 corresponds to digit 0 on a 7 segment display. The segments A to G on the display should be connected to PORTDs pins 0-6
 char numbermap[] = {	0x00,
-						0x3f,
-						0x06,
-						0x5b,
-						0x4f,
-						0x66,
-						0x6d,
-						0x7d,
-						0x07,
-						0x7f,
-						0x6f,
-						0x00};
+			0x3f,
+			0x06,
+			0x5b,
+			0x4f,
+			0x66,
+			0x6d,
+			0x7d,
+			0x07,
+			0x7f,
+			0x6f,
+			0x00};
+
+// outputs a digit to the 7 segment display, if the segments A to G are connected to PORTDs pins 0-6
+void set_7_display(unsigned char number) {
+	PORTD = numbermap[number];
+}
+
+// reads state of input pin
+char read_input(void) {
+	return PINB & (1 << 4);
+}
+
+// to enable multiple digits displayed, which I have not implemented
+void set_display_position(uint8_t pos) {
+	PORTB = 0x00 | (1 << pos);
+}
 
 int main (void) {
 	// Port D 0-6 output
 	DDRD = 0x7f;
 
 	// Pin B4 input
-	DDRB &= ~(1 << 4);
+	// DDRB &= ~(1 << 4);
 
-	// PB4 låg, ingen pullup
+	// PB 0-3 outputs, PB 4 input
+	DDRB = 0x0f;
+
+	// PB4 low, no pullup
 	PORTB &= ~(1 << 4);
-
-	// clear display
-	// PORTD = 0; //numbermap[4];
 
 	state_dial current_state = START;
 	state_dial next_state = START;
@@ -49,28 +73,10 @@ int main (void) {
 	uint64_t timer = 0;
 	uint64_t timeout = 225000;
 
-	// debug
-	DDRB |= (1 << 5);
-	// PORTB &= ~(1 << 5);
+	int posidx = 0;
+ 	while(1) {
 
-	// while(!read_input());
-
-	// PORTB |= (1 << 5);
-	// while(1);
-
-
-
-
-
-
-	while(1) {
-
-		// timer++;
-		// if(timer > 225000) {
-		// 	PORTB ^= (1 << 5);
-		// 	timer = 0;
-		// }
-
+		// finite state machine for main operation
 		switch(current_state) {
 			case START:
 				timer = 0;
@@ -87,7 +93,7 @@ int main (void) {
 					next_state = INPUT_LOW;
 					counter++;
 
-					_delay_ms(100);
+					_delay_ms(75);
 				}
 				if(timer > timeout) {
 					next_state = START;
@@ -114,21 +120,7 @@ int main (void) {
 
 		current_state = next_state;
 
-		set7diplay(counter);
-
-		// if(display_number > 0) {
-		// 	set7diplay(display_number - 1);
-		// } else {
-		// 	set7diplay(display_number);
-		// }
-		// set7diplay(display_number);
+		// outputs entered number. Counts up, so if 2 is entered, first 0, then 1, then 2 is displayed
+		set_7_display(counter);
 	}
-}
-
-void set7diplay(unsigned char number) {
-	PORTD = numbermap[number];
-}
-
-char read_input(void) {
-	return PINB & (1 << 4);
 }
